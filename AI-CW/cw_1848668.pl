80185019b037983eda97f25d1cd1f8db4bbc9c55
Jarrod Doyle <thejarroddoyle@gmail.com>
2020-12-03 18:14:34 +0000

    Part1: Replaced DFS with BFS

--- AI-CW/cw_1848668.pl
+++ AI-CW/cw_1848668.pl
@@ -1,15 +1,20 @@
 % Accomplish a given Task and return the Cost
 solve_task(Task,Cost) :-
     my_agent(A), get_agent_position(A,P),
-    solve_task_dfs(Task,[P],[P|Path]), !,
+    solve_task_bfs(Task, [P:[]],[],[P|Path]), !,
     agent_do_moves(A,Path), length(Path,Cost).
 
 % Calculate the path required to achieve a Task
-solve_task_dfs(Task,[P|Ps],Path) :-
-    achieved(Task,P), reverse([P|Ps],Path)
+solve_task_bfs(Task, [Pos:RPath|Queue],Visited,Path) :-
+    achieved(Task, Pos), reverse([Pos|RPath],Path)
     ;
-    map_adjacent(P,Q,empty), \+ member(Q,Ps),
-    solve_task_dfs(Task,[Q,P|Ps],Path).
+    findall(NewPos:[Pos|RPath], (
+            map_adjacent(Pos,NewPos,empty),
+            \+ member(NewPos,Visited),
+            \+ member(NewPos:_,Queue)
+    ),Children),
+    append(Queue,Children,NewQueue),
+    solve_task_bfs(Task, NewQueue,[Pos|Visited],Path).
 
 % True if the Task is achieved with the agent at Pos
 achieved(Task,Pos) :- 
