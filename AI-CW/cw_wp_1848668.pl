c0504eaa4fe3d7d4f46ac34042d0f6917ab46216
Jarrod Doyle <thejarroddoyle@gmail.com>
2020-12-07 16:59:17 +0000

    Basic greedy approach to Part2

--- AI-CW/cw_wp_1848668.pl
+++ AI-CW/cw_wp_1848668.pl
@@ -3,16 +3,19 @@ actor_has_link(L,A) :-
     actor(A), wp(A,WT), wt_link(WT,L).
 
 % Attempt to solve by visiting each oracle in ID order
-eliminate(As,A,K) :- 
+eliminate(As,A) :-
     As=[A], !
     ;
-    solve_task(find(o(K)),_), !,
-    my_agent(N),
+    my_agent(N), get_agent_position(N, P),
+    findall(K, (
+        solve_task_bfs(find(o(K)), [_:P:[]], [], [P | Path]),
+        \+ agent_check_oracle(N, o(K)), !
+        ), [K | _]),
+    solve_task(find(o(K)),_),
     agent_ask_oracle(N,o(K),link,L), 
     include(actor_has_link(L),As,ViableAs), 
-    K1 is K+1, 
-    eliminate(ViableAs,A,K1).
+    eliminate(ViableAs,A).
 
 % Deduce the identity of the secret actor A
 find_identity(A) :- 
-    findall(A,actor(A),As), eliminate(As,A,1).
+    findall(A,actor(A),As), eliminate(As,A).
