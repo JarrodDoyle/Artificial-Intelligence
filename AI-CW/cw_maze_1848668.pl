0b21390d031f05fb2b8de86426353f6f8c0c7334
Jarrod Doyle <thejarroddoyle@gmail.com>
2020-12-08 19:11:12 +0000

    Multi agents now path properly Currently however they all move on separate ticks instead of doing a batch move. They also don't exit the maze yet.

--- AI-CW/cw_maze_1848668.pl
+++ AI-CW/cw_maze_1848668.pl
@@ -6,24 +6,26 @@ build_Vs_and_Ps([A|As], [[P]|Ps]) :-
     build_Vs_and_Ps(As, Ps).
 
 
-find_moves([], [], [], [], [], []).
+find_moves([], [], [], [], []).
 
-find_moves([A|As], [_|Vs], [[P|_]|Ps], [_|NewVs], [_|NewPs], [_|Ms]) :-
-    ailp_grid_size(N), P = p(N,N), leave_maze(A), !,
-    find_moves(As, Vs, Ps, NewVs, NewPs, Ms).
-
-find_moves([A|As], [V|Vs], [[P|RPath]|Ps], [[M|V]|NewVs], [[M,P|RPath]|NewPs], [M|Ms]) :-
+find_moves([A|As], [V|Vs], [[P|RPath]|Ps], [[M|V]|NewVs], [[M,P|RPath]|NewPs]) :-
     findall(X, (
         agent_adjacent(A, X, OID),
-        OID \= t(_),
+        OID = empty,
         \+ member(X, V)
     ), PosMs),
     random_member(M, PosMs),
-    find_moves(As, Vs, Ps, NewVs, NewPs, Ms).
+    agent_do_moves(A, [M]),
+    find_moves(As, Vs, Ps, NewVs, NewPs).
 
-find_moves([_|As], [V|Vs], [[_|RPath]|Ps], [V|NewVs], [RPath|NewPs], [M|Ms]) :-
+find_moves([A|As], [V|Vs], [[_|RPath]|Ps], [V|NewVs], [RPath|NewPs]) :-
     split_list(RPath, M, _),
-    find_moves(As, Vs, Ps, NewVs, NewPs, Ms).
+    lookup_pos(M, empty),
+    agent_do_moves(A, [M]),
+    find_moves(As, Vs, Ps, NewVs, NewPs).
+
+find_moves([_|As], [V|Vs], [P|Ps], [V|NewVs], [P|NewPs]) :-
+    find_moves(As, Vs, Ps, NewVs, NewPs).
 
 solve_maze :-
     my_agents(As),
@@ -32,7 +34,7 @@ solve_maze :-
     solve_maze_multi_agent(As, VsAndPs, VsAndPs, p(N,N)).
 
 solve_maze_multi_agent(As, Vs, Ps, Exit) :-
-    find_moves(As, Vs, Ps, NewVs, NewPs, Moves),
-    agents_do_moves(As, Moves),
+    find_moves(As, Vs, Ps, NewVs, NewPs),
+    % agents_do_moves(As, Moves),
     % Check if any are at the end!!!"!"£!£$%!$
     solve_maze_multi_agent(As, NewVs, NewPs, Exit).
